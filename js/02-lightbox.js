98afc9694974749bea5b89ee8c5b4fc305858be5
NatolokaS <129997302+NatolokaS@users.noreply.github.com>
2023-08-20 01:53:55 +0300

    task02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+const galleryCardMarkup = renderList(galleryItems);
+
+function renderList(galleryItems) {
+  return galleryItems
+    .map(
+        (item) => `<li class="item gallery__item">
+      <a class="gallery__link" href="${item.original}" >
+  <img class="gallery__image" alt="${item.description}" src="${item.preview}" width="300" />
+  </a>
+  </li>`
+    )
+  .join("");
+};
+
+listEl.insertAdjacentHTML("beforeend", galleryCardMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
+
+
